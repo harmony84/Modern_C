e7a68e868a5d6316b25d221343dc9d6d652e3797
harmony84 <harmonic84@gmail.com>
2017-11-12 03:34:00 +0900

    Selection sort algorism

---
+++ Sort/Selection/Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#pragma region "Selection Sort"  
+// compare count = N2/2
+// swap count = N   
+// selection sort = O(N2)
+template<typename T>
+void SelectionSort(T& target, bool order)
+{
+	size_t count = strlen(target);
+
+	for ( size_t i = 0; i < count - 1; ++i )
+	{
+		int target_value = target[ i ];
+		int target_index = i;
+
+		for ( size_t j = i + 1; j < count; ++j )
+		{
+			if (order == true )
+			{
+				if ( target_value > target[ j ] )
+				{
+					target_value = target[ j ];
+					target_index = j;
+				}
+			}
+			else
+			{
+				if ( target_value < target[ j ] )
+				{
+					target_value = target[ j ];
+					target_index = j;
+				}
+			}
+		}
+
+		target[ target_index ] = target[ i ];
+		target[i] = target_value;
+	}
+}
+#pragma endregion
+
+
+int main()
+{
+#pragma region "Selection Sort"
+	char word[] = { "TOLEARNSORTALGORITHM" };
+	SelectionSort( word, true );
+#pragma endregion
+	return 0;
+}
