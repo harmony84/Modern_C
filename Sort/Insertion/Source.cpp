c4a7aa336b8ab75cafde05336fd72579f8ac358d
harmony84 <harmonic84@gmail.com>
2017-11-30 20:29:08 +0900

    Sort - Insertion

--- Sort/Insertion/Source.cpp
+++ Sort/Insertion/Source.cpp
@@ -4,54 +4,24 @@
 #pragma region "Insertion Sort"  
 // compare = a little
 // insert = a lot of
- //insertion sort = O(N2)
-void InsertionSort( char target[], size_t count, bool order )
+ // insertion sort = O(N^2)
+void InsertionSort( char* target, size_t count )
 {
-	for ( size_t i = 1; i < count; ++i )
+	for ( size_t i = 0; i < count - 1; ++i )
 	{
-		int target_value = target[ i ];
-		size_t j = i;
-
-		if ( order == true )
+		for (size_t j = i+1; j > 0; --j)
 		{
-			while ( target[ j - 1 ] > target_value && j > 0 )
+			if ( target[ j - 1 ] > target[ j ] )
 			{
-				target[ j ] = target[ j - 1 ];
-				--j;
+				char temp = target[ j - 1 ];
+				target[ j - 1 ] = target[ j ];
+				target[ j ] = temp;
+			}
+			else
+			{
+				break;
 			}
 		}
-		else
-		{
-			while ( target[ j - 1 ] < target_value && j > 0 )
-			{
-				target[ j ] = target[ j - 1 ];
-				--j;
-			}
-		}
-
-		target[ j ] = target_value;
-	}
-}
-
-void IndirectInsertionSort( char target[], std::unique_ptr<int[]>& index, size_t count, bool order)
-{
-	for ( size_t i = 0; i < count; ++i )
-	{
-		index[ i ] = i;
-	}
-
-	for ( size_t i = 1; i< count; ++i)
-	{
-		int target_value = index[ i ];
-		size_t j = i;
-
-		while ( j > 0 && target[ index[ j - 1 ] ] > target[ target_value ] )
-		{
-			index[ j ] = index[ j - 1 ];
-			--j;
-		}
-
-		index[ j ] = target_value;
 	}
 }
 #pragma endregion
@@ -61,27 +31,9 @@ int main()
 {
 #pragma region "Insertion Sort"
 	char word[] = { "TOLEARNSORTALGORITHM" };
-	size_t count = strlen( word );
-	//InsertionSort( std::add_lvalue_reference_t<char[]>( word ), count, true );
+	InsertionSort( word, strlen(word) );
 
-	// indirect sort (integer index sort)
-	std::unique_ptr<int[]> word_index = std::make_unique<int[]>( count );
-	IndirectInsertionSort( std::add_lvalue_reference_t<char[]>( word ), word_index, count, true );
-
-	for ( size_t i = 0; i < count; ++i )
-	{
-		std::cout << word_index[ i ] << " ";
-	}
-
-	std::cout << std::endl;
-
-	for ( size_t i = 0; i < count; ++i )
-	{
-		std::cout << word[ word_index[ i ] ];
-	}
-
-	std::cout << std::endl;
-
+	printf( "%s\n", word );
 #pragma endregion
 	return 0;
 }
