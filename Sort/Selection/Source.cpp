b129dc36cb35a54e2054d0e5169f9ad0056cab0d
harmony84 <harmonic84@gmail.com>
2017-11-30 20:21:19 +0900

    Sort - Selection

--- Sort/Selection/Source.cpp
+++ Sort/Selection/Source.cpp
@@ -4,35 +4,19 @@
 // compare count = N2/2
 // swap count = N   
 // selection sort = O(N2)
-void SelectionSort(char* target, size_t count, bool order)
+void SelectionSort( char* target, size_t count )
 {
 	for ( size_t i = 0; i < count - 1; ++i )
 	{
-		int target_value = target[ i ];
-		int target_index = i;
-
 		for ( size_t j = i + 1; j < count; ++j )
 		{
-			if (order == true )
+			if ( target[ i ] > target[ j ] )
 			{
-				if ( target_value > target[ j ] )
-				{
-					target_value = target[ j ];
-					target_index = j;
-				}
-			}
-			else
-			{
-				if ( target_value < target[ j ] )
-				{
-					target_value = target[ j ];
-					target_index = j;
-				}
+				char temp = target[ i ];
+				target[ i ] = target[ j ];
+				target[ j ] = temp;
 			}
 		}
-
-		target[ target_index ] = target[ i ];
-		target[i] = target_value;
 	}
 }
 #pragma endregion
@@ -42,7 +26,8 @@ int main()
 {
 #pragma region "Selection Sort"
 	char word[] = { "TOLEARNSORTALGORITHM" };
-	SelectionSort( word, strlen(word), true );
+	SelectionSort( word, strlen( word ) );
+	printf( "%s\n", word );
 #pragma endregion
 	return 0;
 }
